62b71b969c234c6910cb18d8a2dbafe11ea9735c
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2023-12-23 13:59:51 +0530

    practice array coding ninja

---
+++ Array Question/reverse-kth-pos.cpp
@@ -0,0 +1,15 @@
+//Reverse the array from the Kth postion 
+#include<bits/stdc++.h>git 
+using namespace std;
+int main()
+{
+    vector<int>v;
+    for(int i =0; i<6; i++){
+        int element;
+        cin>>element;
+        v.push_back(element);
+    }
+    for(int ele : v){
+        cout << ele<<" ";
+    }
+}
