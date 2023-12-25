aaa0ebd38258afd72ccf24d18d68f2262ece8246
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2023-12-25 11:59:50 +0530

    play with vector

--- practice.cpp
+++ practice.cpp
@@ -3,14 +3,11 @@ using namespace std;
 int main(){
     int arr[] ={12,32,10,34,6,4,36};
     int size=sizeof(arr)/sizeof(arr[0]);
-    int x =90;
-    int ans =34;
-    for(int i = 0; i<size; i++){
-        if(arr[i]==x)
-            ans = i;
-            break;
-
+    int i = 0;
+    swap(arr[i] , arr[size-1]);
+    for(i = 0; i<size; i++){
+        cout << arr[i]<<" ";
     }
-    cout <<"Ans "<<ans;
+    
 
 }
