485ed3b7fdf1db26c9867bf0e4fa09dd44479dc2
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2023-12-27 23:53:55 +0530

    wrong o/p

--- Q3_set.cpp
+++ Q3_set.cpp
@@ -1,43 +1,46 @@
-/*
-check if string has all english alphabets
-Given a string.You have to check if it has all english alphabets from a-z.
-
-Input 1:abcdEFGHIJKLmnopqrstuvWXYZ
-Output1:Yes
-
-Input 2:PhysicsWallah
-output 2: No
-
-Explanation:
-Input has all the alphabets irrespective of uppercase or lowercase, so the output is Yes.But in case of Input-2. It doesn't contain all the alphabets, hence No. 
-*/
-#include<bits/stdc++.h>
 #include<iostream>
-#include<set>
 using namespace std;
-bool checkAllAlphabets(string s){
-    if(s.length()<26){
-        return false;
-    }
-    transform(s.begin(), s.end(),s.begin(),::tolower); // conve
-
-    set<char>alphabets;
-    for(auto ch:s){
-        alphabets.insert(ch);
-    }
-
-    return (alphabets.size()==26);
-
-}
 int main(){
-    string input;
-    cin >> input;
-
-    if(checkAllAlphabets(input)){
-        cout <<"Yes";
+    int arr[] = {1,2,3,4,5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0; i<size; i++){
+        cout << arr[i]<<" ";
     }
-    else{
-        cout <<"No";
+
+    cout << endl;
+    int ele , index;
+     
+    cin >> ele;
+    cin >> index;
+    for(int i = size+1; i>index; i--){
+        arr [i]= arr[i-1];
+        
+    }
+    arr[index] = ele;
+    
+    for(int i = 0; i<size+1; i++){
+        cout << arr[i]<<" ";
     }
     return 0;
 }
+//User function Template for C++
+
+class Solution{
+  public:
+    // You need to insert the given element at the given index. 
+    // After inserting the elements at index, elements
+    // from index onward should be shifted one position ahead
+    // You may assume that the array already has sizeOfArray - 1
+    // elements.
+    void insertAtIndex(int arr[], int sizeOfArray, int index, int element)
+    {
+              for(int i=sizeOfArray-1 ; i >= index; i--){
+            arr[i+1] = arr[i];
+    
+       }
+       arr[index] = element;       
+    
+        
+        
+    }
+};
--- practice.cpp
+++ practice.cpp
@@ -1,83 +1,26 @@
-// // #include<iostream>
-// // #include<set>
-// // #include<vector>
-// // using namespace std;
-// // int main(){
-// //     // set<string>s1;
-// //     // int n;
-// //     // cin >> n;
-// //     // string name;
-// //     // while(n--){
-// //     //     getline(cin, name);
-// //     //     s1.insert(name);
-// //     // }
-// //     // cout <<"Final Invite list: ";
-// //     // for(auto name:s1){
-// //     //     cout <<name<<endl;
-// //     // }
-// //     // int m , n;
-// //     // cout << "Size of m and n: "<<endl;
-// //     // cin >> m >> n;
-
-// //     // vector<int> v1(m);
-// //     // vector<int> v2(n);
-
-// //     // cout <<"Enter the Elements in the v1: ";
-// //     // for(int i = 0;i<m; i++){
-// //     //     cin>>v1[i];
-// //     // }
-
-// //     // cout<<"Entet the Elements in the v2: ";
-// //     // for(int i = 0; i<n; i++){
-// //     //     cin>>v2[i];
-// //     // }
-// //     // set<int>s1;
-// //     // for(auto ele : v1){
-// //     //     s1.insert(ele);
-// //     // }
-// //     // int sum= 0;
-// //     // for(auto ele :v2){
-// //     //     if(s1.find(ele)!= s1.end()){
-// //     //         sum = sum+ele;//sum+= ele;
-// //     //     }
-// //     // }
-
-// //     // cout <<"After the adding the common Element: "<<sum;
-
-// //     // return 0;
-    
-// //  }
-
-// #include<bits/stdc++.h>
-// using namespace std;
-// bool checkAllAlphabets(string name){
-//     if(name.length()<26){
-//         return false;
-//     }
-//     transform(name.begin(), name.end(),name.begin(),::toupper);
-//     set<char>s1;
-//     for(auto ch : name){
-//         s1.insert(ch);
-//     }
-//     return (s1.size()==26);
-// }
-// int main(){
-//     string name;
-//     cin>>name;
-//     if(checkAllAlphabets(name)){
-//         cout <<"Yes";
-//     }
-//     else{
-//         cout <<"No";
-//     }
-//     return 0;
-// }
-#include<bits/stdc++.h>
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
-    string s ;
-    getline (cin , s);
-    transform(s.begin(),s.end(),s.begin(),::toupper);
-    cout << s;
+    vector<int>v(6);
+    cout<<"Enter the Elements: "<<endl;
+    for(int i = 0; i<v.size(); i++){
+        cin >> v[i];
+    }
+    cout <<"Enter the last occurence of the Element: ";
+    int x; 
+    cin >> x;
+
+    for(int i = 0; i<v.size(); i++){
+        if(v[i]==x){
+            cout <<"Element found: ";
+            
+        }
+        else{
+            cout <<"Element not found: ";
+        }
+        
+    }
+
+    return 0;
 }
