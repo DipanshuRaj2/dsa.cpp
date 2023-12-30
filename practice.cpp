18107d055e3163d668a84683b2c24e10ac55897f
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2023-12-30 13:16:02 +0530

    array manipulation

--- practice.cpp
+++ practice.cpp
@@ -1,39 +1,37 @@
 #include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
+int getSecondOrderElements(int n, vector<int> v) {
+    // Write your code here.
+    int max =INT_MIN;
+    int min  =INT_MIN;
+    int maxIndex = -1;
+    int minIndex = -1;
+    for(int i = 0; i<n; i++){
+        if(v[i]>max){
+            maxIndex =i;
+        }
+        if(v[i]<min){
+            minIndex = v[i];
+        }
+    }
+    v[maxIndex] ,v[minIndex] = -1;
+    max = INT_MIN;
+    min = INT_MAX;
+    for(int i = 0; i<n; i++){
+        if(v[i]>max){
+            max = v[i];
+        }
+        if(v[i]<min){
+            min = v[i];
+        }
+    }
+    return {max,min} ;
+}
 int main(){
-    vector<int>v(6);
-    cout<<"Enter the Elements: "<<endl;
-    for(int i = 0; i<v.size(); i++){
-        cin >> v[i];
-    }
-    cout <<"Enter the Element which is greater than x: ";
-    int x; 
-    cin >> x;
-
-    bool found = false;
-    for(int i = 0; i<v.size(); i++){
-        if(v[i]==x){
-            found = true;
-            break;
-        }
-      
-    }
-    if(found){
-        int count=0;
-
-    for(int i = v.size()-1; i>=0; i--){
-        if(v[i]>x){
-            count++;
-            
-        }
-    }
-    cout<<"total no. of occurence of given element: "<<count;
-    }
-    else{
-        cout <<"Element not found: ";
-    }
-    
-
+    vector<int>v={12, 23, 4, 5, 7, 61, 23};
+    int n = v.size();
+    cout<< getSecondOrderElements(n , v);
     return 0;
 }
+//hello hi
