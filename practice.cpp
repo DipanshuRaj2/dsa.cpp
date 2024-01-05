4aadad8e4d8d4bcf8ba04e517906d4bb0dd011aa
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-05 23:11:38 +0530

    previous commit

--- practice.cpp
+++ practice.cpp
@@ -1,15 +1,32 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    vector<int>v ={1,2,3,2,1,3,3,4};
-    sort(v.begin(),v.end());
-    int unique = 0;
+int largestElementIndex(vector<int>v){
+    int ans  = INT_MIN;
+    int index= -1;
     for(int i = 0; i<v.size(); i++){
-        if(v[i] != v[i+1]){
-            unique = v[i];
+        if(v[i]>ans){
+            ans = v[i];
+            index = i;
         }
     }
-    cout << unique ;
+    return index;
+}
+using namespace std;
+int main(){
+    vector<int>v ={1,2,3,2,4,3,3,4,4};
+    int largestIndex = largestElementIndex(v);
+    // int largest = v[largestElementIndex];
+    cout <<v[largestIndex]<<endl;
+    int largestElement = v[largestIndex];
+    for(int i = 0; i<v.size(); i++){
+        if(v[i] == largestElement){
+            v[i] =-1;
+        }
+    }
+    // v[largest] = -1;
+    int secondLargest = largestElementIndex(v);
+    cout <<v[secondLargest];
+
     return 0;
 }
