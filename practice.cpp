af3d5de58782f903145ae7aa1aeae149d855dbb1
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-13 09:10:57 +0530

    two pointer

--- practice.cpp
+++ practice.cpp
@@ -1,40 +1,12 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 using namespace std;
-void countZero(vector<int>&v){
-    int count =0;
-    for(int i = 0; i<v.size(); i++){
-        if(v[i]==0){
-            count++;
-        }
+int main()
+{  
+    int n =2;
+    vector <int>v ={0,1};
+    for(int i =0; i<n; i++){
+        
     }
-    for(int i = 0; i<v.size(); i++){
-        if(i<count){
-            v[i] = 0;
-        }
-        else{
-            v[i] = 1;
-        }
-    }
-}
-int main(){
-    // sort the array by 0 and 1;
-    int n;
-    cout<<"Enter the size of the array: "<<endl;
-    cin >> n;
-    vector<int>v;
-
-    cout<<"Enter the Element in the array: ";
-    for(int i = 0; i<n; i++){
-        int element; cin>>element;
-        v.push_back(element);
-    }
-
-    countZero(v);
-    for(int i = 0; i<n; i++){
-        cout<<v[i]<<" ";
-    }
-
-    return 0;
+   return 0;
 }
