e0758d46829540f86c7addfd0b148060a9c0c81f
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-14 13:12:08 +0530

    sorted the array of non-negative and square

--- ArrayQ.(medium)(two-pointer)/Sorted-the-square.cpp
+++ ArrayQ.(medium)(two-pointer)/Sorted-the-square.cpp
@@ -1,3 +1,19 @@
+//Given an Integer array 'a' sorted in non-decreasing order, return an array of each number sorted in non-decreasing order.
+// ex = 1 {-10, -8, -3, 4, 5, 6};
+// o/p ={9, 16, 25, 36, 64, 100,};
+
+
+
+
+
+
+
+
+
+
+
+
+
 // #include<iostream>
 // #include<vector>
 // #include<algorithm>
@@ -28,36 +44,39 @@
 //another approach
 #include<iostream>
 #include<vector>
+#include<bits/stdc++.h>
 using namespace std;
-
 void sortTheSquareArray(vector<int>&v){
-    vector<int>ans;
     int left_ptr = 0;
     int right_ptr = v.size()-1;
+    vector<int>ans;
     while(left_ptr<=right_ptr){
-        if(abs(v[left_ptr])<abs(v[right_ptr])){
-            ans = v[left_ptr]*v[left_ptr];
 
+        if(abs(v[left_ptr]) < abs(v[right_ptr])){
+            ans.push_back(v[right_ptr]*v[right_ptr]);
+            right_ptr--;
         }
+
         else{
-            ans  = v[right_ptr]*v[right_ptr];
+            ans.push_back(v[left_ptr]*v[left_ptr]);
+            left_ptr++;
         }
     }
+    reverse(ans.begin(),ans.end());
+    for(int i = 0; i<v.size(); i++){
+        cout << ans[i]<<" "; 
+    }
+    cout<<endl;
 }
 int main(){
     int n;
-    cin>>n;
-    vector<int>v;
-
-    for(int i = 0; i<v.size(); i++){
+    cin >> n;
+    vector<int> v;
+    for(int i = 0; i<n; i++){
         int ele; cin>>ele;
         v.push_back(ele);
     }
-
     sortTheSquareArray(v);
-
-    for(int i = 0; i<v.size(); i++){
-        cout<<v[i]<<" ";
-    }
+    
     return 0;
 }
