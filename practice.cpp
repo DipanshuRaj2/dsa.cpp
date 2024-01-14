2e93a44d911690b6139b3ec39b7f4ee89c818434
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-14 16:10:28 +0530

    pracrice

--- practice.cpp
+++ practice.cpp
@@ -1,12 +1,31 @@
+//target sum
 #include<iostream>
 #include<vector>
 using namespace std;
+
 int main()
-{  
-    int n =2;
-    vector <int>v ={0,1};
-    for(int i =0; i<n; i++){
+{
+    int n;
+    cin >> n;
+    vector<int>v;
+    for(int i = 0; i<n; i++){
+        int ele; cin >> ele;
+        v.push_back(ele);
+    }
+    int count = 0;
+    int target = 1;
+    for(int i = 0; i<n; i++){
+        for(int j = i+1; j<n; j++){
+            if(i == j){
+                continue;
+            }
+            if(v[i]+v[j] == target){
+                cout << v[i]<<" "<<v[j];
+            }
+
+        }
         
     }
+    
    return 0;
-} 
+}
