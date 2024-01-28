78c17b900f7b54bafc259ef19554df7833d3c080
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-28 16:06:29 +0530

    practice

--- function/hello.cpp
+++ function/hello.cpp
@@ -1,16 +1,97 @@
+// #include<iostream>
+// using namespace std;
+// int powerOf_a_and_b(){
+//     int a ,b;
+//     cin >> a >> b;
+//     int ans  = 1;
+//     for(int i = 0; i<b; i++){
+//         ans = ans  * a;
+//     }
+//     return ans;
+// }
+// int main()
+// {
+
+//     cout<<powerOf_a_and_b()<<endl;
+//     cout<<powerOf_a_and_b();
+//    return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// bool isEven(int num){
+//     if(num % 2==0){
+//         return true;
+//     }
+//     return false;
+// } 
+// int main()
+// {
+//     int num;
+//     cin >> num;
+//     if(isEven(num)){
+//         cout<<"number is even :"<<num;
+//     }
+//     else{
+//         cout<<"number is odd: "<<num;
+//     }
+//    return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int factorial(int n){
+//     int ans = 1;
+//     for(int i = 1; i<=n; i++){
+//     ans = ans * i;
+//     }
+//     return ans;
+// }
+// int nCR(int n,int r){
+//     int num = factorial(n);
+//     int deno = factorial(r) * factorial(n-r);
+
+//     return num/deno;
+// }
+
+
+// int main()
+// {
+//     int n , r;
+//     cin >> n >> r;
+//     int ans = nCR(n , r);
+//     cout<<ans;
+//    return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// void counting(int n){
+//     for(int i = 0; i<n; i++){
+//         cout << i<<" ";
+//     }
+// }
+// int main()
+// {
+//     int n ;
+//     cin >> n;
+//     counting(n);
+//    return 0;
+// }
 #include<iostream>
-using namespace std;
-int powerOf_a_and_b(int a , int b){
-    int ans  = 1;
-    for(int i = 0; i<b; i++){
-        ans = ans  * a;
+using namespace std; 
+bool isPrime(int n){
+    for(int i = 2; i<=24; i++){
+        if(n % i == 1){
+            return 0;
+        }
     }
-    return ans;
+    return 1;
 }
-int main()
-{
-    int a , b;
-    cin >> a >> b;
-    cout<<powerOf_a_and_b(a , b);
-   return 0;
+int main(){
+    int n = 27;
+    if(isPrime(n)){
+        cout<<"Prime no: "<< n;
+    }
+    else{
+        cout<<"No prime no: "<<n;
+    }
+    return 0;
 }
--- practice.cpp
+++ practice.cpp
@@ -1,7 +1,45 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    cout<<"hello world";
-   return 0;
+class Solution {
+public:
+    vector<int>getStoreNumber(int n){
+        vector<int> storeNum ;
+        while(n != 0){
+            int number = n % 10 ;
+            storeNum.push_back(number) ;
+            n = n / 10 ;
+        }
+        return storeNum ;
+    }
+    bool isHappy(int n) {
+        if(n < 0) return false ;
+        vector<int>storeNumber = getStoreNumber(n) ;
+        int sum = 0 ;
+        
+        while(sum != 1){
+            for(int i = 0 ; i < storeNumber.size() ; i++){
+                sum += (storeNumber[i] * storeNumber[i]) ;
+            }
+
+            storeNumber.clear() ;
+            storeNumber = getStoreNumber(sum) ;
+            if(sum == 1){
+                return true ;
+            }
+            if(sum == 4) return false ;
+            sum = 0 ;
+        }
+        return true ;
+    }
+};
+
+int main(){
+    int n ;
+
+    cin>>n;
+
+    Solution s ;
+    cout<<s.isHappy(n)<<endl;
+    
+    return 0;
 }
