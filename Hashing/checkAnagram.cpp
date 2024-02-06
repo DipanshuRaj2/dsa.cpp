1445ee5f885bbcc136b168b3cc485371a2ad7ae3
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-02-06 10:19:30 +0530

    check Anagram

--- Hashing/checkAnagram.cpp
+++ Hashing/checkAnagram.cpp
@@ -29,38 +29,65 @@
 //    return 0;
 // }
 
+// #include<bits/stdc++.h>
+// using namespace std;
+// bool checkAnagram(string s1, string s2){
+//     unordered_map<char , int>m1;
+//     unordered_map<char , int>m2;
+
+//     for(int i = 0; i<s1.length(); i++){
+//         m1[s1[i]]++;
+//     }
+
+//     for(int i = 0; i<s2.length(); i++){
+//         m2[s2[i]]++;
+//     }
+
+//     for(auto ele: m1){
+//         cout<<ele.first<<" "<<ele.second<<endl;
+//     }
+
+//     if(m1 == m2){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+
+
+
+// }
+// int main()
+// {
+//     string s1 = "rat";
+//     string s2 = "cat";
+//     cout<<(checkAnagram(s1 ,s2)?"yes" : "No");
+//    return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 bool checkAnagram(string s1, string s2){
-    unordered_map<char , int>m1;
-    unordered_map<char , int>m2;
+    unordered_map<char, int> m;
 
     for(int i = 0; i<s1.length(); i++){
-        m1[s1[i]]++;
+        m[s1[i]]++;
     }
-
     for(int i = 0; i<s2.length(); i++){
-        m2[s2[i]]++;
+        m[s2[i]]--;
     }
 
-    for(auto ele: m1){
-        cout<<ele.first<<" "<<ele.second<<endl;
+    for(auto &ele : m){
+        if(ele.second !=0){
+            return false;
+        }
     }
-
-    if(m1 == m2){
-        return true;
-    }
-    else{
-        return false;
-    }
-
-
-
+    return true;
 }
 int main()
 {
     string s1 = "triangle";
     string s2 = "integral";
-    cout<<(checkAnagram(s1 ,s2)?"yes" : "No");
+    cout<<(checkAnagram(s1,s2)?"Yes" : "No");
    return 0;
 }
